4972c8f130dfed69faf6328f75bdcdb97c83e65f
makoto hata <makotohata0429@gmail.com>
2024-09-26 07:51:21 +0900

    bookingsテーブルを追加

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 
@@ -12,6 +13,8 @@ class User(Base):
     updated_at = Column(
         DateTime, default=datetime.now(), onupdate=datetime.now())
 
+    bookings = relationship("Booking", back_populates="user")
+
 
 class Room(Base):
     __tablename__ = "rooms"
@@ -22,3 +25,21 @@ class Room(Base):
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(
         DateTime, default=datetime.now(), onupdate=datetime.now())
+
+    bookings = relationship("Booking", back_populates="room")
+
+
+class Booking(Base):
+    __tablename__ = "bookings"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    room_id = Column(
+        Integer, ForeignKey("rooms.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(
+        DateTime, default=datetime.now(), onupdate=datetime.now())
+
+    user = relationship("User", back_populates="bookings")
+    room = relationship("Room", back_populates="bookings")
