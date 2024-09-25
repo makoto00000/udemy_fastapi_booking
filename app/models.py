e2e5c22df1c1a620b74ee3d1109e9ad692fa845b
makoto hata <makotohata0429@gmail.com>
2024-09-26 07:48:22 +0900

    roomsテーブルを追加

--- app/models.py
+++ app/models.py
@@ -11,3 +11,14 @@ class User(Base):
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(
         DateTime, default=datetime.now(), onupdate=datetime.now())
+
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    capacity = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(
+        DateTime, default=datetime.now(), onupdate=datetime.now())
