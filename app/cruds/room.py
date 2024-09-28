46f9f15165c7aaedf20d27d82ed26db7d262f60b
makoto hata <makotohata0429@gmail.com>
2024-09-28 18:58:53 +0900

    部屋一覧取得機能を追加

--- app/cruds/room.py
+++ app/cruds/room.py
@@ -3,6 +3,10 @@ from models import Room
 from schemas import RoomCreate
 
 
+def find_all(db: Session):
+    return db.query(Room).all()
+
+
 def create(db: Session, room_create: RoomCreate):
     new_room = Room(**room_create.model_dump())
     db.add(new_room)
--- app/routers/room.py
+++ app/routers/room.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from starlette import status
 from cruds import room as room_cruds
 from schemas import RoomResponse, RoomCreate
 from database import get_db
@@ -11,6 +12,13 @@ DbDependency = Annotated[Session, Depends(get_db)]
 router = APIRouter(prefix="/room", tags=["Rooms"])
 
 
-@router.post("", response_model=RoomResponse, status_code=201)
+@router.get(
+    "", response_model=list[RoomResponse], status_code=status.HTTP_200_OK)
+async def find_all(db: DbDependency):
+    return room_cruds.find_all(db)
+
+
+@router.post(
+    "", response_model=RoomResponse, status_code=status.HTTP_201_CREATED)
 async def create(db: DbDependency, room_create: RoomCreate):
     return room_cruds.create(db, room_create)
